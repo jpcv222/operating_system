1c3f7ae79f0cf54fdf88cbae00442dbd5589d855
Invitado <invitado@s2pc19.eisc.univalle.edu.co>
2019-07-29 21:00:45 -0500

    Add semaphore implementation

--- ccode/account.cpp
+++ ccode/account.cpp
@@ -14,13 +14,73 @@ File account.c
 
 // Create a random sleep-time (to induce unpredictability)
 static int duration(int min, int max){
+	 static bool first_time = true; 
+    if(first_time){
+        first_time =  false;
+        srand48((int) time(NULL));
+    }
+
+    return (int) (min + drand48() * (max - min));
 }
 
-void proc_A(int *account, int loops){ 
+void proc_A(int *account, int loops){
+	int temp; 
+	for (int i ; i< loops; i++){
+		printf("Agrego 200 \n", );
+		temp = *account + 200; 
+		sleep(duration(1,2));
+		printf("Estado de cuenta = %d\n", temp);
+		} 
 }
 
 void proc_B(int *account, int loops){ 
+	int temp; 
+	for (int i ; i< loops; i++){
+		printf("Agrego 300 \n", );
+		temp = *account + 300; 
+		sleep(duration(1,2));
+		printf("Estado de cuenta = %d\n", temp);
+		} 
 }
 
 int main(void){
+	Shared_memory allow_account;
+	int *status_account;
+	int pid;
+	int status;
+	
+	allow_account = Shared_memory_create(sizeof(int));
+	status_account = (int*) Shared_memory_attach (allow_account); 
+	*status_account = 100000;
+	
+	pid = fork();
+	
+	if(pid==0){
+		proc_A(status_account, 5);
+		Shared_memory_dettach(status_account);
+		exit(0); 
+		
+		}else{
+		printf("Parent: first child created = %d \n", pid);
+			pid = fork();
+		
+			if(pid==0){
+			proc_B(status_account, 5);
+			Shared_memory_dettach(status_account);
+			exit(0); 
+			
+			}else{
+			printf("Parent: first child created = %d \n", pid);
+			
+				}
+			}
+			
+			pid = wait(&status);
+			printf("Parent: first child terminated = %d \n", pid);
+			pid = wait(&status);
+			printf("Parent: first child terminated = %d \n", pid);
+			
+			printf("Parent: status_account = %d \n", *status_account);
+			exit(0);
+	
 }
--- ccode/synchro.cpp
+++ ccode/synchro.cpp
@@ -1,4 +1,8 @@
+
 /*---------------------------------------------------------------
+File: Synchro.cpp
+Author: Juan Pablo Castro <jpcv222>
+Date: 29-07-2019
 -----------------------------------------------------------------*/
 
 #include "semaphore.h" // You will need this to use semaphores
@@ -12,17 +16,63 @@
 
 // Create a random sleep-time (to induce unpredictability)
 static int duration(int min, int max){
-	exit(0);
+    static bool first_time = true; 
+    if(first_time){
+        first_time =  false;
+        srand48((int) time(NULL));
+    }
+
+    return (int) (min + drand48() * (max - min));
 }
 
 void proc_A(int loops){
-	exit(0);
+    for(int i=0; i<loops; i++){
+        printf("Soy A iteración %d\n", i);
+        sleep(1);
+    }
 }
 
 void proc_B(int loops){
-	exit(0);
+    for(int i=0; i<loops; i++){
+        printf("Soy B iteración %d\n", i);
+        sleep(1);
+    }
 }
 
 int main(void){
-   exit(0);
+
+   Semaphore sem = Semaphore_create(1);
+	
+   int pid;
+   int status;
+   
+   pid = fork();
+
+   if(pid == 0){
+        proc_A(5);
+	Semaphore_wait(sem);
+        exit(0);
+   }
+   else {
+	printf("Parent: second child created = %d \n", pid);
+
+ 	pid = fork();
+	if(pid == 0){
+		proc_B(5);
+		Semaphore_signal(sem);
+		exit(0);
+	   }
+	   else {
+		printf("Parent: second child created = %d \n", pid);
+	   }
+   }
+
+	pid = wait(&status);
+	printf("Parent: first child terminated = %d \n", pid);
+	pid = wait(&status);
+	printf("Parent: first child terminated = %d \n", pid);
+	exit(0);
+	
 }
+
+
